ccfea4bb259a67bfd305e097c233cca2f932a251
Digolas.Forge <42858482+l1jer@users.noreply.github.com>
2025-01-02 23:00:35 +1100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4F46E5',
+        secondary: '#10B981',
+      }
+    },
+  },
+  plugins: [],
+}
